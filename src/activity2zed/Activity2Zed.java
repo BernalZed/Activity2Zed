e20edff8b425c8df86f09e12682e9de8d389393f
Lenovo <Lenovo@LAPTOP-H5VF19G9>
2024-03-15 15:44:44 +0800

    Activity 2

---
+++ src/activity2zed/Activity2Zed.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package activity2zed;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Activity2Zed {
+
+    
+    public static void main(String[] args) {
+        int[] Elements = {10, 20, 30, 40, 50};
+        
+        
+        
+        System.out.println("Print Array =  " + Arrays.toString(Elements));
+        System.out.println("First Element: " + Elements[0]);
+        System.out.println("Second Element: " + Elements[1]);
+        System.out.println("Third Element: " + Elements[2]);
+        System.out.println("Fourth Element: " + Elements[3]);
+        System.out.println("Fifth Element: " + Elements[4]);
+        
+        
+        int sum = Elements[0] + Elements[1] + Elements[3] + Elements[4];
+        System.out.println("\nThe sum is : " + sum);
+        
+        int max; 
+        max = Math.max(Elements[0], Math.max(Elements[1], Math.max(Elements[2], Math.max(Elements[3], 
+                Elements[4]))));
+        
+        System.out.println("Maximum Value: " + max);
+    
+    }
+    
+}
+    
+    
+
